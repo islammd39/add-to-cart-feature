fbad24b8ab43acee7c6915644a39d97db189cc58
islammd39e <islammdrahat308@gmail.comm>
2023-01-05 17:18:06 +0600

    package download

---
+++ src/Root/Root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Pages/Shared/navbar';
+
+
+const Root = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+          
+        </div>
+    );
+};
+
+export default Root;
